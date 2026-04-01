01bea5745d27b846bbd925d4bbd62bacb289722c
Nicolas Agustin Tribolo <151692353+TRIBOLONICOLASAGUSTIN@users.noreply.github.com>
2026-03-31 23:30:19 -0300

    Add files via upload

--- PaginaWeb/Configuracion/Configuracion.js
+++ PaginaWeb/Configuracion/Configuracion.js
@@ -1,4 +1,5 @@
 const btnn= document.getElementById('Modo-Boton');
+const icono = document.getElementById('icono-modo');
 const body= document.body;
 
 if(localStorage.getItem('modo')=='oscuro'){
@@ -10,7 +11,20 @@ btnn.addEventListener('click',()=>{
 
     if(body.classList.contains('dark-mode')){
         localStorage.setItem('modo','oscuro');
+            icono.src = 'Imagen/Luna.svg';
+            icono.alt = 'Modo Noche';
     } else{
         localStorage.setItem('modo','claro');
+            icono.src = 'Imagen/Dia.svg';
+            icono.alt = 'Modo Día';
+    }
+
+    if (body.classList.contains('dark-mode')) {
+    icono.src = 'Imagen/Luna.svg';
+    icono.alt = 'Modo Noche';
+    } 
+    else {
+    icono.src = 'Imagen/Dia.svg';
+    icono.alt = 'Modo Día';
     }
 });
